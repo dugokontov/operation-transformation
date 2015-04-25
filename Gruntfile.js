f696171b83c4e187a6d25dc3cf90d88c983937fa
Sergej Dugokontov <sergejspam@gmail.com>
2015-04-25 21:14:01 +0200

    Get GLU using npm

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,49 +1,24 @@
-//var es6ify = require('es6ify');
-
 module.exports = function (grunt) {
     'use strict';
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         browserify: {
-            glu: {
-                src: ['src/glu.js'],
-                dest: 'demo/glu.bundle.js',
-                options: {
-                    browserifyOptions: {
-                        standalone: 'GLU',
-                        debug: true
-
-                    },
-                    transform: ['babelify'],
-                    watch: true,
-                    postBundleCB: function (err, src, next) {
-                        if (err) {
-                            throw err;
-                        }
-                        var through = require('through');
-                        var stream = through().pause().queue(src).end();
-                        var buffer = '';
-
-                        stream.pipe(require('mold-source-map').transformSourcesRelativeTo(__dirname + '/demo')).pipe(through(function (chunk) {
-                            buffer += chunk.toString();
-                        }, function () {
-                            next(err, buffer);
-                        }));
-                        stream.resume();
-                    }
-                }
-            },
-            demo: {
-                src: ['app/app.js'],
-                dest: 'demo/app.bundle.js',
+            app: {
+                src: ['src/app.js'],
+                dest: 'demo/js/app.bundle.js',
                 options: {
                     browserifyOptions: {
                         standalone: 'APP',
+                        entry: 'src/app.js',
                         debug: true
-
                     },
-                    transform: ['babelify'],
+                    transform: [
+                        'babelify',
+                        [ 'detachkify', {
+                            relativeTo: __dirname + "/src"
+                        }]
+                    ],
                     watch: true,
                     postBundleCB: function (err, src, next) {
                         if (err) {
@@ -53,7 +28,7 @@ module.exports = function (grunt) {
                         var stream = through().pause().queue(src).end();
                         var buffer = '';
 
-                        stream.pipe(require('mold-source-map').transformSourcesRelativeTo(__dirname + '/demo')).pipe(through(function (chunk) {
+                        stream.pipe(require('mold-source-map').transformSourcesRelativeTo(__dirname + '/demo/js')).pipe(through(function (chunk) {
                             buffer += chunk.toString();
                         }, function () {
                             next(err, buffer);
@@ -69,41 +44,14 @@ module.exports = function (grunt) {
                     port: 5555,
                     base: 'demo',
                     middleware: function (connect) {
-                        var proxy = require('grunt-connect-proxy/lib/utils').proxyRequest;
+                        var modRewrite = require('connect-modrewrite');
                         return [
-                            // Include the proxy first
-                            proxy,
-                            // Serve static files.
+                            modRewrite(['!\\.html|\\.js|\\.svg|\\.css|\\.png|\\.jpg|\\.ttf|\\.eot|\\.woff|\\.woff2$ /index.html [L]']),
                             connect['static']('demo')
                         ];
                     }
-                },
-                proxies: [
-                    {
-                        context: '/geoserver',
-                        host: 'spice.socialexplorer.com',
-                        port: 80,
-                        https: false,
-                        changeOrigin: false,
-                        xforward: false
-                    },
-                    {
-                        context: '/SpiceMap',
-                        host: 'spice.socialexplorer.com',
-                        port: 80,
-                        https: false,
-                        changeOrigin: false,
-                        xforward: false
-                    }
-                ]
+                }
             }
-        },
-        //watch: {
-        //files: ['demo/arc.bundle.js'],
-        //tasks: ['jsbeautifier', 'browserify']
-        //},
-        concurrent: {
-            watchAndServer: ['watch', 'connect:server:keepalive']
         },
         yuidoc: {
             compile: {
@@ -117,37 +65,50 @@ module.exports = function (grunt) {
                 }
             }
         },
-        jshint: {
-            allFiles: [
-                'Gruntfile.js',
-                'src/**/*.js'
-//                '!src/project/*.js',
-                //'demo/demoApp.js'
-            ],
-            options: grunt.file.readJSON('.jshintrc')
+        eslint: {
+            target: ['src/**/*.js']
         },
-        jsbeautifier: {
-            src: ['src/**/*.js'],
+        injector: {
             options: {
-                js: {
-                    braceStyle: 'collapse',
-                    breakChainedMethods: false,
-                    e4x: false,
-                    evalCode: false,
-                    indentChar: ' ',
-                    indentLevel: 0,
-                    indentSize: 4,
-                    indentWithTabs: false,
-                    jslintHappy: true,
-                    keepArrayIndentation: false,
-                    keepFunctionIndentation: false,
-                    maxPreserveNewlines: 2,
-                    preserveNewlines: true,
-                    spaceBeforeConditional: true,
-                    spaceInParen: false,
-                    unescapeStrings: false,
-                    wrapLineLength: 0
+                template: "template.html",
+                destFile: "demo/index.html",
+                ignorePath: 'demo/',
+                addRootSlash: false
+            },
+            local_dependencies: {
+                files: {
+                    'demo/index.html': [
+                        'demo/css/bower.bundle.css',
+                        'demo/js/bower.bundle.js',
+                        'demo/js/app.bundle.js'
+                    ]
                 }
+            }
+        },
+        bower_concat: {
+            all: {
+                dest: 'demo/js/bower.bundle.js',
+                cssDest: 'demo/css/bower.bundle.css'
+            }
+        },
+        bower: {
+            fonts: {
+                dest: 'demo/',
+                fonts_dest: 'demo/',
+                skipPatterns: [
+                    /^.*\.js$/i,
+                    /^.*\.css$/i
+                ],
+                options: {
+                    keepExpandedHierarchy: true
+                }
+            }
+        },
+        copy: {
+            assets: {
+                files: [
+                    {expand: true, cwd: 'assets/', src: ['**'], dest: 'demo/'}
+                ]
             }
         }
     });
@@ -157,13 +118,13 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-connect-proxy');
-    grunt.loadNpmTasks('grunt-concurrent');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-jsbeautifier');
+    grunt.loadNpmTasks('grunt-eslint');
+    grunt.loadNpmTasks('grunt-injector');
+    grunt.loadNpmTasks('grunt-bower-concat');
+    grunt.loadNpmTasks('grunt-bower');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     // Default task(s).
-    grunt.registerTask('default', []);
-    grunt.registerTask('deploy', ['browserify']);
-    grunt.registerTask('dev', ['browserify:glu', 'browserify:demo', 'connect:server:keepalive']);
+    grunt.registerTask('deploy', ['browserify', 'bower_concat', 'injector', 'bower:fonts', 'copy:assets']);
+    grunt.registerTask('dev', ['eslint', 'browserify', 'bower_concat', 'injector', 'bower:fonts', 'copy:assets', 'connect:server:keepalive']);
 };
