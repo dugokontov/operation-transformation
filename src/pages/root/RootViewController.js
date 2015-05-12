36c358802426182a61625e90248338263a985df4
Sergej Dugokontov <sergejspam@gmail.com>
2015-05-12 23:11:47 +0200

    Redirect to projects page

--- src/pages/root/RootViewController.js
+++ src/pages/root/RootViewController.js
@@ -4,6 +4,16 @@ class RootViewController extends GLU.ViewController {
   constructor(view) {
     super(view);
   }
+
+  static beforeRouteInvoke(params, next) {
+    if (params.handlerCount === 1) {
+      let routes = require('/routes');
+
+      routes.navigateTo('/projects');
+    } else {
+      next();
+    }
+  }
 }
 
 export default RootViewController;
