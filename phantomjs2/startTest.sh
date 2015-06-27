29dd5b718c3e4ad1ecd7669ecf7071ddd99d8f9d
Sergej Dugokontov <sergejspam@gmail.com>
2015-06-27 13:05:21 +0200

    Enable variable number of calls

--- phantomjs2/startTest.sh
+++ phantomjs2/startTest.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
 rm -f *.csv
-./phantomjs openBrowser.js http://dugokontov.noip.me/#/project/$1 US &
+for i in $(seq 1 $2)
+do
+    ./phantomjs openBrowser.js http://dugokontov.noip.me/#/project/$1 US &
+done
+
