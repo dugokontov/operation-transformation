58fb4b7306ea69e5a9652cda82a3c0b0ab1824b7
Sergej Dugokontov <sergejspam@gmail.com>
2015-06-27 13:19:35 +0200

    Clone repo to get script... but... wouldn't I have repo if script is run

--- phantomjs2/install.sh
+++ phantomjs2/install.sh
@@ -1,5 +1,3 @@
-git clone https://github.com/dugokontov/operation-transformation.git
 sudo apt-get install build-essential g++ flex bison gperf ruby perl \
   libsqlite3-dev libfontconfig1-dev libicu-dev libfreetype6 libssl-dev \
   libpng-dev libjpeg-dev python libX11-dev libxext-dev
-cd operation-transformation/phantomjs2
